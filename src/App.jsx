825ac62871278e8f6dc85092ebe9f295768a15e7
prabhatshu <shuklaprabhatprabhat@gmail.com>
2025-04-17 12:38:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Landing from "./components/Landing";
+import Header from "./components/Header";
+import './App.css'
+function App() {
+
+  return (
+    <div>
+    <Header></Header>
+    <Landing></Landing>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+       
+      <nav className='head'>
+      <h2>HMS</h2>
+       
+           <a href=''>Home</a>
+           <a href=''>Colleges</a>
+           <a href=''>Services</a>
+           <a href=''>Contact us</a>
+    
+      </nav>
+    </div>
+  )
+}
+
+export default Header
